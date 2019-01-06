33be499198fc1242332aaf31cdea889f0ae66e06
Ng Cheng Leong <renlore@hotmail.com>
2019-01-06 12:01:56 +0800

    Updated README

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 dirinfo
 =======
-A Rust library for generating information about a directory.
 
-### Example
+Crate `dirinfo` provides easy to use API for collecting various information about a
+directory hierarchy.
 
-The following code will search through the /etc and all its subdirectories 
-and calculate the total file size in bytes for files with the extension ".conf"
+To use this crate, add `dirinfo` as a dependency to your project's `Cargo.toml`:
+
+``` toml
+[dependencies]
+dirinfo = { git = "https://github.com/renl/dirinfo" }
+```
+
+# Example
+
+The following code calculates the total size in bytes of all files found in the
+directory hierarchy with the root directory specified by the pull function.
+
 ```
 use dirinfo::DirInfo;
 
-println!("{}", DirInfo::new().pull("/etc").get_files_size_by_file_ext(".conf");
+println!("{}", DirInfo::new().pull(".").get_files_size());
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,28 @@
 /*!
- * Crate `dirinfo` provides easy to use API for collecting various information about a
- * directory hierarchy.
- *
- * To use this crate, add `dirinfo` as a dependency to your project's `Cargo.toml`:
- *
- * ``` toml
- * [dependencies]
- * dirinfo = { git = "https://github.com/renl/dirinfo" }
- * ```
- *
- * # Example
- * 
- * The following code calculates the total size in bytes of all files found in the
- * directory hierarchy with the root directory specified by the pull function.
- * 
- * ```
- * use dirinfo::DirInfo;
- *
- * println!("{}", DirInfo::new().pull(".").get_files_size());
- * ```
- */
+dirinfo
+=======
+
+Crate `dirinfo` provides easy to use API for collecting various information about a
+directory hierarchy.
+
+To use this crate, add `dirinfo` as a dependency to your project's `Cargo.toml`:
+
+``` toml
+[dependencies]
+dirinfo = { git = "https://github.com/renl/dirinfo" }
+```
+
+# Example
+
+The following code calculates the total size in bytes of all files found in the
+directory hierarchy with the root directory specified by the pull function.
+
+```
+use dirinfo::DirInfo;
+
+println!("{}", DirInfo::new().pull(".").get_files_size());
+```
+*/
 use walkdir::{DirEntry, WalkDir};
 #[cfg(test)]
 mod tests;
