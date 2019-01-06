9e2cb2ed08573bd6f111bb595756ad36588a518d
Ng Cheng Leong <renlore@hotmail.com>
2019-01-06 11:58:11 +0800

    Added more documentation and remove redundent pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+/*!
+ * Crate `dirinfo` provides easy to use API for collecting various information about a
+ * directory hierarchy.
+ *
+ * To use this crate, add `dirinfo` as a dependency to your project's `Cargo.toml`:
+ *
+ * ``` toml
+ * [dependencies]
+ * dirinfo = { git = "https://github.com/renl/dirinfo" }
+ * ```
+ *
+ * # Example
+ * 
+ * The following code calculates the total size in bytes of all files found in the
+ * directory hierarchy with the root directory specified by the pull function.
+ * 
+ * ```
+ * use dirinfo::DirInfo;
+ *
+ * println!("{}", DirInfo::new().pull(".").get_files_size());
+ * ```
+ */
 use walkdir::{DirEntry, WalkDir};
 #[cfg(test)]
 mod tests;
@@ -9,14 +31,14 @@ pub enum BlockSize {
 }
 
 #[derive(Debug)]
-pub struct Error {
+struct Error {
     error: Option<std::io::Error>,
     path: Option<String>,
     depth: usize,
 }
 
 impl Error {
-    pub fn new(path: Option<String>, depth: usize, error: Option<std::io::Error>) -> Error {
+    fn new(path: Option<String>, depth: usize, error: Option<std::io::Error>) -> Error {
         Error {
             path: path,
             depth: depth,
